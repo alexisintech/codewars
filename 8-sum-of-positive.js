4fca04e9eaa771ebb634e3fa2988463cc9e7cfbf
Alexis Aguilar <alexmerona@gmail.com>
2022-04-21 21:31:38 -0400

    Update 8-sum-of-positive.js

--- 8-sum-of-positive.js
+++ 8-sum-of-positive.js
@@ -35,3 +35,17 @@ function positiveSum(arr) {
 }
 
 */
+
+/* OTHER SOLUTIONS ~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+function positiveSum(arr) {
+  return arr.reduce((a,b) => a + (b > 0 ? b : 0), 0);
+}
+
+~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+function positiveSum(arr) {
+  return arr.filter( x => x > 0).reduce((a,b) => a+b, 0);
+}
+
+*/
