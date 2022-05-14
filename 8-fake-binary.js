56a707e0bdc3d7f6b7f58de55ee54f300f0ea32f
Alexis Aguilar <alexmerona@gmail.com>
2022-05-13 21:34:44 -0400

    Create 8-fake-binary.js

---
+++ 8-fake-binary.js
@@ -0,0 +1,38 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Fake Binary
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/57eae65a4321032ce000002d
+
+
+// INSTRUCTIONS
+/* 
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+*/
+
+
+// ~*~*~*~*~*~* MY SOLUTION
+
+function fakeBin(x){
+    return x.split('').map( x => x < 5 ? 0 : 1 ).join('')
+  }
+    
+/* ~*~*~*~*~*~* TOP CODE WARS SOLUTION
+   
+function fakeBin(x){
+    return x.split('').map( x => x < 5 ? 0 : 1 ).join('')
+  }
+
+*/
+  
+
+/* ~*~*~*~*~*~*  OTHER SOLUTIONS
+
+
+
+*/
