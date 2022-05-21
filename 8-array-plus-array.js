a61fcddc3a8fe8e2758f413e0e11e1b1cd8961e7
Alexis Aguilar <alexmerona@gmail.com>
2022-05-20 22:08:33 -0400

    Create 8-array-plus-array.js

---
+++ 8-array-plus-array.js
@@ -0,0 +1,34 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Array plus array
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+
+
+// INSTRUCTIONS
+/* 
+I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+*/
+
+
+// ~*~*~*~*~*~* MY SOLUTION
+
+const arrayPlusArray = (arr1, arr2) => {return arr1.reduce((a,c) => a + c, 0) + arr2.reduce((a,c) => a + c, 0);}
+    
+/* ~*~*~*~*~*~* TOP CODE WARS SOLUTION
+   
+
+
+*/
+  
+
+/* ~*~*~*~*~*~*  OTHER SOLUTIONS
+
+
+
+*/
