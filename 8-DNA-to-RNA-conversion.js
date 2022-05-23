783e2ccfb3633644a5028755766d8fe5eb053c69
Alexis Aguilar <alexmerona@gmail.com>
2022-05-22 23:31:35 -0400

    Create 8-DNA-to-RNA-conversion.js

---
+++ 8-DNA-to-RNA-conversion.js
@@ -0,0 +1,49 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// DNA to RNA conversion
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/5556282156230d0e5e000089
+
+
+// INSTRUCTIONS
+/* 
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+*/
+
+
+// ~*~*~*~*~*~* MY SOLUTION
+
+function DNAtoRNA(dna) {
+    let arr = dna.split('');
+    let rna = arr.map(letter => {
+      return letter === 'T' ? 'U' : letter
+    })
+    return rna.join('');
+  }
+    
+/* ~*~*~*~*~*~* TOP CODE WARS SOLUTION
+   
+function DNAtoRNA(dna) {
+  return dna.split("T").join("U");
+}
+
+*/
+  
+
+/* ~*~*~*~*~*~*  OTHER SOLUTIONS
+
+
+
+*/
