bee475067f980756a9af6340d1d16015f749811c
Alexis Aguilar <alexmerona@gmail.com>
2022-05-24 22:06:42 -0400

    Create 8-sum-arrays.js

---
+++ 8-sum-arrays.js
@@ -0,0 +1,34 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Sum Arrays
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/53dc54212259ed3d4f00071c/javascript
+
+
+// INSTRUCTIONS
+/* 
+
+*/
+
+
+// ~*~*~*~*~*~* MY SOLUTION
+
+const sum = numbers => {return numbers.length === 0 ? 0 : numbers.reduce((a,c) => a + c, 0)}
+    
+/* ~*~*~*~*~*~* TOP CODE WARS SOLUTION
+   
+function sum(numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
+}
+
+*/
+  
+
+/* ~*~*~*~*~*~*  OTHER SOLUTIONS
+
+
+
+*/
