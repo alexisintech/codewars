01da489012d6b01e667f4559865a9dcb4715c107
Alexis Aguilar <alexmerona@gmail.com>
2022-05-28 22:03:20 -0400

    Create 8-reduce-but-grow.js

---
+++ 8-reduce-but-grow.js
@@ -0,0 +1,34 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Beinnger - Reduce but grow
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+
+
+// INSTRUCTIONS
+/* 
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+
+
+// ~*~*~*~*~*~* MY SOLUTION
+
+const grow = x => {return x.reduce((a,c) => a * c, 1)}
+    
+/* ~*~*~*~*~*~* TOP CODE WARS SOLUTION
+   
+
+
+*/
+  
+
+/* ~*~*~*~*~*~*  OTHER SOLUTIONS
+
+
+
+*/
