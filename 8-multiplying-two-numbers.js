d6e9c7259dfd701bd533ff9d40aee931639e1f85
Alexis Aguilar <alexmerona@gmail.com>
2022-06-01 21:47:16 -0400

    Create 8-multiplying-two-numbers.js

---
+++ 8-multiplying-two-numbers.js
@@ -0,0 +1,32 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Function 3 - multiplying two numbers
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/523b66342d0c301ae400003b
+
+
+// INSTRUCTIONS
+/* 
+Implement a function which multiplies two numbers.
+*/
+
+
+// ~*~*~*~*~*~* MY SOLUTION
+
+const multiply = (x,y) => {return x*y}
+    
+/* ~*~*~*~*~*~* TOP CODE WARS SOLUTION
+   
+
+
+*/
+  
+
+/* ~*~*~*~*~*~*  OTHER SOLUTIONS
+
+
+
+*/
