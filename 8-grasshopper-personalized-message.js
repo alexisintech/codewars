75bae5eadc2958df479856c1b948edda8aa8bf31
Alexis Aguilar <alexmerona@gmail.com>
2022-06-04 22:04:45 -0400

    Create 8-grasshopper-personalized-message.js

---
+++ 8-grasshopper-personalized-message.js
@@ -0,0 +1,38 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Grasshopper - Personalized Message
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7
+
+
+// INSTRUCTIONS
+/* 
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise
+*/
+
+
+// ~*~*~*~*~*~* MY SOLUTION
+
+const greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest'
+    
+/* ~*~*~*~*~*~* TOP CODE WARS SOLUTION
+   
+
+
+*/
+  
+
+/* ~*~*~*~*~*~*  OTHER SOLUTIONS
+
+
+
+*/
