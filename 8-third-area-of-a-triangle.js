aed11f786852a86706bb7fd7dd3e99a7e987ee1d
Alexis Aguilar <alexmerona@gmail.com>
2022-06-10 22:58:31 -0400

    Create 8-third-area-of-a-triangle.js

---
+++ 8-third-area-of-a-triangle.js
@@ -0,0 +1,38 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Third Angle of a Triangle
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/5a023c426975981341000014
+
+
+// INSTRUCTIONS
+/* 
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle
+*/
+
+
+// ~*~*~*~*~*~* MY SOLUTION
+
+const otherAngle = (a, b) => 180 - (a+b)
+    
+/* ~*~*~*~*~*~* TOP CODE WARS SOLUTION
+   
+
+
+*/
+  
+
+/* ~*~*~*~*~*~*  OTHER SOLUTIONS
+
+
+
+*/
