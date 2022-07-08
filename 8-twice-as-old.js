fdb147c566784e552d1bb4fc8826f5b86dd9d737
Alexis Aguilar <alexmerona@gmail.com>
2022-07-08 00:17:56 -0400

    Create 8-twice-as-old.js

---
+++ 8-twice-as-old.js
@@ -0,0 +1,36 @@
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+//////////////////////////////////////////
+// Twice as old
+//////////////////////////////////////////
+// ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ *
+
+// URL
+// https://www.codewars.com/kata/5b853229cfde412a470000d0
+
+
+// INSTRUCTIONS
+/* 
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+*/
+
+
+// ~*~*~*~*~*~* MY SOLUTION
+
+const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs((sonYearsOld*2)-dadYearsOld)
+
+/* ~*~*~*~*~*~* TOP CODE WARS SOLUTION
+   
+const twiceAsOld = (d, s) => Math.abs(d - 2 * s);
+
+*/
+  
+
+/* ~*~*~*~*~*~*  COMMENTS:
+
+
+
+*/
